2ebe8c3176bfff6b5d66359403fe6cce08442d09
Nagaveni345 <naninagaveni1438@gmail.com>
2026-06-08 20:52:26 +0530

    ewz

---
+++ Student.java
@@ -0,0 +1,22 @@
+class Student
+{
+int id;
+String name;
+Student(int id,String name)
+{
+this.id=id;
+this.name=name;
+}
+void display()
+{
+System.out.println( thisid,thisname);
+}
+}
+class Demo
+{
+public static void main(String[]args)
+{
+Student S=new Student(101,Nagaveni);
+S.display();
+}
+}
